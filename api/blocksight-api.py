84b9ee92ecfdf9655f98e47f4cd9fdbfb126844b
Bosun Akinyemi <azurnwa.p@gmail.com>
2024-04-07 13:53:16 +0100

    cors fix for localhost

--- api/blocksight-api.py
+++ api/blocksight-api.py
@@ -2,6 +2,11 @@ from walletVettingModule.wallet_vetting_utils import process_wallet
 from flask import Flask
 
 app = Flask(__name__)
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
 
 @app.route("/core/analyse-wallet/<wallet_address>")
 async def analyse_wallet(wallet_address):
