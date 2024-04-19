c2dd21d335ee2c0e48b9e8b40c477140c99f4d9d
web3dozie <web3dozie@gmail.com>
2024-04-19 10:10:29 -0400

    Small API Changes

--- usersModule/user_utils.py
+++ usersModule/user_utils.py
@@ -314,6 +314,12 @@ async def discord_command_executor(text: str, user: discord.User, client: discor
     elif text.startswith('.scan '):
         split_text = text.split()
         data_to_scan = split_text[1]
+        try:
+            window = int(split_text[2])
+            if window not in [1, 7, 30]:
+                raise KeyError
+        except KeyError:
+            window = 30
 
         scan_embed = Embed(color=0xc8a2c8, title=f'Starting scan for {data_to_scan[0:5]}...',
                            description='Please be patient')
@@ -325,20 +331,34 @@ async def discord_command_executor(text: str, user: discord.User, client: discor
 
         if is_valid_wallet(data_to_scan):
             scan_message = await message.channel.send(content='', embed=scan_embed)
+
+            def make_wallet_scan_embed(wallet_data):
+                wallet_data = {'avg_size': 807.06,
+                               'last_checked': 1713531556,
+                               'pnl': 2191.04,
+                               'trades': 5,
+                               'wallet': '3AL3N6WgbyMX8XpAV7TSJrHdDxQNDX7R1j5neXVAQVxA',
+                               'win_rate': 0.0,
+                               'window_value': '30d'}
+
+                grade = ''
+
+                wallet_scan_embed = Embed(color=0xc8a2c8, title=f"{wallet_data['wallet'][0:7]}...'s  {window}D Summary",
+                                          description='In-Depth Breakdown')
+                wallet_scan_embed.set_footer(text=f"BlockSight",
+                                             icon_url="https://cdn.discordapp.com/attachments/"
+                                                      "1184131101782970398/1189235897288372244/BSL_Gradient.png")
+
             # TODO check if wallet exists in db, if it is new increase points by 10
             async with aiohttp.ClientSession() as session:
-                async with session.get(f"{blocksight_api}/core/analyse-wallet/{data_to_scan}") as response:
+                async with session.get(f"{blocksight_api}/core/analyse-wallet/{data_to_scan}",
+                                       params={'window': window}) as response:
                     if response.status == 200:
                         result = await response.json()
                         summary = result.get("result")
 
-                        data = {'avg_size': 807.06,
-                                'last_checked': 1713531556,
-                                'pnl': 2191.04,
-                                'trades': 5,
-                                'wallet': '3AL3N6WgbyMX8XpAV7TSJrHdDxQNDX7R1j5neXVAQVxA',
-                                'win_rate': 0.0,
-                                'window_value': '30d'}
+
+
 
 
                     else:
