1c70a4040d99df1c54e68908b11653199eacd39e
Bosun Akinyemi <azurnwa.p@gmail.com>
2024-06-24 20:43:24 +0100

    tweak deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 cd ..
+rm -rf ./BlockSightV3Deploy
 cp -r ./BlockSightV3 ./BlockSightV3Deploy
 cd ./BlockSightV3Deploy
 rm -rf venv
+rm -rf .git
 cd ..
 tar -czf bs.tar.gz ./BlockSightV3Deploy
 scp ./bs.tar.gz root@62.171.171.121:/root/blocksight
+rm -rf ./BlockSightV3Deploy
