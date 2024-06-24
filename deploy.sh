a216f6ab3d4261e0dfd95a6c921bc68d09c4cccc
Bosun Akinyemi <azurnwa.p@gmail.com>
2024-06-24 20:03:43 +0100

    update shell scripts

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 cd ..
-tar -czf bs.tar.gz ./BlockSightV3
+cp -r ./BlockSightV3 ./BlockSightV3Deploy
+cd ./BlockSightV3Deploy
+rm -rf venv
+cd ..
+tar -czf bs.tar.gz ./BlockSightV3Deploy
 scp ./bs.tar.gz root@62.171.171.121:/root/blocksight
